5a668d1e0695588f7582dfe9c864156053a53d51
unknown <hibari100206@gmail.com>
2017-07-25 18:16:49 +0900

    プログラムの作成

---
+++ prg/src/main.c
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+			
+
+int main(void)
+{
+
+	for(;;);
+}
+
+/* -------------------------------------------------
+ * @ä÷êîñº		:	DipSW_init
+ * @äTóv		:	DipSWÇ…égÇ§GPIOÇÃèâä˙âª
+ * @ñﬂÇËíl		:	Ç»Çµ
+ * ---------------------------------------------- */
+void RotarySW_init(void)
+{
+	GPIO_InitTypeDef init_gpio;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	/*DipSW*/
+	GPIO_StructInit(&init_gpio);
+	init_gpio.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12;	//
+	init_gpio.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Init(GPIOA,&init_gpio);
+
+	return;
+}
